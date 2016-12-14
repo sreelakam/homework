757d2d2b366dfd6964169e7e5f5fd8a9f7c0df21
athira <athujadhu1@gmail.com>
2016-12-14 14:56:48 +0800

    renamed

--- fizzbizz.py
+++ fizzbizz.py
@@ -1,10 +1,10 @@
 def fizzbizz(n):
     for i in range(1,n+1):
         if i % 3==0 and i % 5==0:
-           print("fizzbizz".format(i))
+           print("fizzbizz")
         elif i % 5==0:
-             print("bizz".format(i))
+             print("bizz")
         elif i % 3==0:
-             print("fizz".format(i))
+             print("fizz")
         else:
             print(i)
